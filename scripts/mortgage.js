3704d9c469b919fbc53bd491d8babadbf21080e2
ojohnson1 <ojohnson659@gmail.com>
2023-10-23 14:30:04 -0400

    fied mortgage misspelling and finished mort. calc

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -12,5 +12,33 @@ function init(){
 
 function buttonCalculateClick(){
 
+const inputPrincipal =document.getElementById("inputPrincipal");
+const inputInterest =document.getElementById("inputInterest");
+const inputLoanLength =document.getElementById("inputLoanLength");
+
+
+
+let principal= Number(inputPrincipal.value);
+let yearlyInterest=Number(inputInterest.value);
+let loanLengthInYears= Number(inputLoanLength.value);
+
+let monthlyInterestAsFraction = yearlyInterest / 100 / 12;
+let loanLengthInMonths=loanLengthInYears*12;
+
+
+let monthlyPayment= principal * ( (monthlyInterestAsFraction)/(1-(1+ monthlyInterestAsFraction)** -loanLengthInMonths));
+   monthlyPayment= monthlyPayment.toFixed(2)
+  
+  
+   let totalAmount=(monthlyPayment * loanLengthInMonths)-principal;
+
+
+
+
+const outputMonthlyPayment=document.getElementById('outputMonthlyPayment');
+const outputTotalAmount=document.getElementById("outputTotalAmount");
+
+outputMonthlyPayment.value =monthlyPayment;
+outputTotalAmount.value =totalAmount;
 
 }
