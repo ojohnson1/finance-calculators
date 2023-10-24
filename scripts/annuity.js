626879665b95173adf62b753926ab12104acfe80
ojohnson1 <ojohnson659@gmail.com>
2023-10-24 08:34:31 -0400

    finished annuity calculator

--- scripts/annuity.js
+++ scripts/annuity.js
@@ -1,1 +1,39 @@
 "use strict"
+
+
+
+window.onload =init;
+
+
+function init(){
+    const buttonCalculate= document.getElementById("buttonCalculate")
+    buttonCalculate.onclick = buttonCalculateClick;
+
+}
+
+function buttonCalculateClick (){
+
+// Get the starting values
+  //inputMonthlyPayout
+  //inputInterestRate
+  //inputAmountOfYears 
+  const inputMonthlyPayout=document.getElementById("inputMonthlyPayout")
+  const inputInterest = document.getElementById('inputInterest');
+  const inputAmountOfYears = document.getElementById('inputAmountOfYears');
+
+//Calculate the unknown values
+
+let monthlyPayout = Number (inputMonthlyPayout.value);
+let interestRate = Number(inputInterest.value);
+let totalTime = Number(inputAmountOfYears.value) ;
+let periodInterestRate= interestRate/100 /12;
+
+let presentValue= monthlyPayout*((1-(1+periodInterestRate)**(-totalTime*12))/periodInterestRate)
+   presentValue=presentValue.toFixed (2);
+//Display Output
+
+//outputPresentValue
+const outputPresentValue=document.getElementById('outputPresentValue')
+ outputPresentValue.value = presentValue
+}
+
